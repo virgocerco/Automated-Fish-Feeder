4dcc19760d933bf2474f3cb1f05627a275f5a0e6
virgocerco <mondegrafe@gmail.com>
2024-12-12 09:02:49 +0800

    ok

--- app/src/utils/firebaseConfig.ts
+++ app/src/utils/firebaseConfig.ts
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getDatabase } from 'firebase/database';
-import firebaseConfig from './config';
+import firebaseConfig from './super/config';
 
 const app = initializeApp(firebaseConfig);
 
