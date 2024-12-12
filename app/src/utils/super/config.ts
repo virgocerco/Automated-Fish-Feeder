b8a8f1306333a3ab20067b710fb549b1c35498e4
virgocerco <mondegrafe@gmail.com>
2024-12-12 09:05:31 +0800

    ok

---
+++ app/src/utils/super/config.ts
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCiltxnDdu0644S3n-a9-4yNe_n2NUGdkw",
+    authDomain: "sixteen-app-db.firebaseapp.com",
+    projectId: "sixteen-app-db",
+    storageBucket: "sixteen-app-db.firebasestorage.app",
+    messagingSenderId: "258138763412",
+    appId: "1:258138763412:web:7ae1ec23da541f65094481",
+    measurementId: "G-9E0G4BK4G7",
+    databaseURL: "https://sixteen-app-db-default-rtdb.asia-southeast1.firebasedatabase.app",
+  };
+  
+  export default firebaseConfig;
+  
